4d299741a22c5730eb3cb45063c26af4f86e86f5
Elie RUGGIERO <elie.ruggiero25@gmail.com>
2023-11-20 07:08:45 +0100

    Update PassWindow.hpp

--- PassWindow.hpp
+++ PassWindow.hpp
@@ -1,17 +1,18 @@
 #ifndef PAMA_PASSWINDOW_H
 #define PAMA_PASSWINDOW_H
 
-#include <QMainWindow.h>
-#include <QGroupBox.h>
 #include <QBoxLayout.h>
-#include <QPushButton.h>
-#include <QLabel.h>
 #include <QCombobox.h>
+#include <QGroupBox.h>
+#include <QLabel.h>
 #include <QListView.h>
+#include <QMainWindow.h>
+#include <QPushButton.h>
+
 #include "crypto.hpp"
-#include "utils.hpp"
-#include "pwidget.hpp"
 #include "pvars.hpp"
+#include "pwidget.hpp"
+#include "utils.hpp"
 
 #include <QDebug.h>
 
@@ -52,6 +53,7 @@ private: //variables
 	QString black = "#0A0905";
 	QString bright_dark = "#262625";
 	QString white = "#FCFCFA";
+	// TODO: change the code to remove the line below
 	QString entryStyle_base = "background-color: %0; color: %1; padding-left: 15px; padding-right: 15px; border-radius: 5px;";  //can the user create a new property in qss ? ::error who can be manage with a bool
 	QString entryStyle;
 
